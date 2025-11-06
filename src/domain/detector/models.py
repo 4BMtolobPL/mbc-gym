c78a8850a33d6a899de9d5b9f303862630fbd94a
4BMtolobPL <k4qe6urml3@gmail.com>
2025-11-06 16:00:00 +0900

    User - UserImage relationship 매핑

--- src/domain/detector/models.py
+++ src/domain/detector/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.main import db
 
@@ -12,8 +12,12 @@ class UserImage(db.Model):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     image_path: Mapped[str] = mapped_column()
     is_detected: Mapped[bool] = mapped_column(default=False)
-    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now(datetime.UTC))
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        default=datetime.datetime.now(datetime.UTC)
+    )
     updated_at: Mapped[datetime.datetime] = mapped_column(
         default=datetime.datetime.now(datetime.UTC),
         onupdate=datetime.datetime.now(datetime.UTC),
     )
+
+    user: Mapped["User"] = relationship(back_populates="images")
