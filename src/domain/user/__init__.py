d0ad1316f5ceac148a022574aa052d681ba02d94
4BMtolobPL <k4qe6urml3@gmail.com>
2025-11-06 10:10:54 +0900

    user, auth 도메인 추가

---
+++ src/domain/user/__init__.py
@@ -0,0 +1,1 @@
+import src.domain.user.models
---
+++ src/domain/user/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+from werkzeug.security import generate_password_hash
+
+from src.main import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, index=True)
+    email = db.Column(db.String, unique=True, index=True)
+    password_hash = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now(datetime.UTC))
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.datetime.now(datetime.UTC),
+        onupdate=datetime.datetime.now(datetime.UTC),
+    )
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
