1099cbedaeb0ffcf889a9951d3196fc74852aa3a
4BMtolobPL <k4qe6urml3@gmail.com>
2025-11-06 16:52:28 +0900

    development 환경 config에 파일 업로드 경로 추가

--- src/config/config.py
+++ src/config/config.py
@@ -23,6 +23,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_ECHO = True
     SECRET_KEY = os.environ.get("SECRET_KEY")
     WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_SECRET_KEY")
+    UPLOAD_FOLDER = str(Path(basedir, "src", "uploads"))
 
 
 class TestingConfig(Config):
