d047e848859cfad71f676cd8f3c3264715f57bf6
4BMtolobPL <k4qe6urml3@gmail.com>
2025-11-06 15:35:45 +0900

    UserImage 모델 추가

---
+++ src/domain/detector/models.py
@@ -0,0 +1,19 @@
+import datetime
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.main import db
+
+
+class UserImage(db.Model):
+    __tablename__ = "user_images"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    image_path: Mapped[str] = mapped_column()
+    is_detected: Mapped[bool] = mapped_column(default=False)
+    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.now(datetime.UTC))
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        default=datetime.datetime.now(datetime.UTC),
+        onupdate=datetime.datetime.now(datetime.UTC),
+    )
