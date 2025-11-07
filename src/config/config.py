b62ce8c51773c417452c055dca6b69a6f09b412f
4BMtolobPL <k4qe6urml3@gmail.com>
2025-11-07 10:32:19 +0900

    모델 다운로드 코드 README.md에 추가, 모델 라벨 config에 추가, 모델명 ignore

--- src/config/config.py
+++ src/config/config.py
@@ -1,12 +1,109 @@
 import os
 from pathlib import Path
+from typing import List
 
 from flask import Config
 
 basedir = Path(__file__).resolve().parent.parent.parent
 
 
-class ProductionConfig(Config):
+class BaseConfig(Config):
+    LABELS: List[str] = [
+        "unlabeled",
+        "person",
+        "bicycle",
+        "car",
+        "motorcycle",
+        "airplane",
+        "bus",
+        "train",
+        "truck",
+        "boat",
+        "traffic light",
+        "fire hydrant",
+        "street sign",
+        "stop sign",
+        "parking meter",
+        "bench",
+        "bird",
+        "cat",
+        "dog",
+        "horse",
+        "sheep",
+        "cow",
+        "elephant",
+        "bear",
+        "zebra",
+        "giraffe",
+        "hat",
+        "backpack",
+        "umbrella",
+        "shoe",
+        "eye glasses",
+        "handbag",
+        "tie",
+        "suitcase",
+        "frisbee",
+        "skis",
+        "snowboard",
+        "sports ball",
+        "kite",
+        "baseball bat",
+        "baseball glove",
+        "skateboard",
+        "surfboard",
+        "tennis racket",
+        "bottle",
+        "plate",
+        "wine glass",
+        "cup",
+        "fork",
+        "knife",
+        "spoon",
+        "bowl",
+        "banana",
+        "apple",
+        "sandwich",
+        "orange",
+        "broccoli",
+        "carrot",
+        "hot dog",
+        "pizza",
+        "donut",
+        "cake",
+        "chair",
+        "couch",
+        "potted plant",
+        "bed",
+        "mirror",
+        "dining table",
+        "window",
+        "desk",
+        "toilet",
+        "door",
+        "tv",
+        "laptop",
+        "mouse",
+        "remote",
+        "keyboard",
+        "cell phone",
+        "microwave",
+        "oven",
+        "toaster",
+        "sink",
+        "refrigerator",
+        "blender",
+        "book",
+        "clock",
+        "vase",
+        "scissors",
+        "teddy bear",
+        "hair drier",
+        "toothbrush",
+    ]
+
+
+class ProductionConfig(BaseConfig):
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
@@ -16,7 +113,7 @@ class ProductionConfig(Config):
     WTF_CSRF_SECRET_KEY = os.environ.get("WTF_CSRF_SECRET_KEY")
 
 
-class DevelopmentConfig(Config):
+class DevelopmentConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'local.sqlite'}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -26,7 +123,7 @@ class DevelopmentConfig(Config):
     UPLOAD_FOLDER = str(Path(basedir, "src", "uploads"))
 
 
-class TestingConfig(Config):
+class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{basedir / 'testing.sqlite'}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
